8ff2d3029c08c27d974724890494aeaae5ab66b6
Martin Stigen <martin.stigen@live.no>
2017-02-10 14:03:28 +0100

    setup webpack

---
+++ react/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    context: __dirname,
+
+    entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+    output: {
+        path: path.resolve('./assets/bundles/'),
+        filename: "bundle.js",
+    },
+
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,         // Match both .js and .jsx files
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query:
+                    {
+                        presets:['react']
+                    }
+            }, // to transform JSX into JS
+        ],
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+}

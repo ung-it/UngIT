18e534812b82aff5072b8e5d3246e427bbaa1875
Martin Stigen <martin.stigen@live.no>
2017-02-16 10:43:06 +0100

    Fixed webpack not bundeling on server

--- assets/js/App.js
+++ assets/js/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h2>React component 1</h2>
+                <h2>React component newwwww</h2>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -6,22 +6,22 @@ module.exports = {
     context: __dirname,
 
     entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
+        //'webpack-dev-server/client?http://localhost:3000',
+        //'webpack/hot/only-dev-server',
         './assets/js/index'
     ],
 
     output: {
-        path: path.resolve('./assets/bundles/'),
+        path: path.resolve('./public/'),
         //filename: '[name]-[hash].js',
         filename: 'bundle.js',
-        publicPath: 'http://localhost:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+        publicPath: 'http://localhost:8080/public/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
-        new BundleTracker({filename: './webpack-stats.json'}),
+        //new webpack.HotModuleReplacementPlugin(),
+        //new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
+        //new BundleTracker({filename: './webpack-stats.json'}),
     ],
 
     module: {

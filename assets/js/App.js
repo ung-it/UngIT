70e90aa62d7352ed446b4c57747e4366bcfe7396
Martin Stigen <martin.stigen@live.no>
2017-02-16 11:38:09 +0100

    Created frontpage, navbar and promovideo

--- assets/js/App.js
+++ assets/js/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h2>React component newwwww</h2>
+                <h2>I'm an activity</h2>
             </div>
         );
     }

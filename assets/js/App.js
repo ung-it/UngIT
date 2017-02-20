dcc195ef6eb660d5439a3a262e2ef1437a999069
Martin Stigen <martin.stigen@live.no>
2017-02-20 15:09:14 +0100

    minor text change

--- assets/js/App.js
+++ assets/js/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h2>React component newwwww</h2>
+                <h2>React component</h2>
             </div>
         );
     }

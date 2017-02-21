9f6ff6db886eeff87315af9e0ebbd299eae3d0f4
Martin Stigen <martin.stigen@live.no>
2017-02-21 12:35:50 +0100

    trying to correct setup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Aktiviteter from './Aktivities';
+import Activities from './Activities';
 
 ReactDOM.render(
-    <Aktiviteter />,
+    <Activities />,
     document.getElementById('activities')
 );

920adf10fe93883921f53aaed3ecb63f462012d6
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-21 14:20:00 +0100

    fixed superuser

--- skalvi/admin.py
+++ skalvi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Users)
+admin.site.register(Activity)
+admin.site.register(Organisation)
+admin.site.register(ParticipateIn)
+admin.site.register(Hosts)
+admin.site.register(EmployedIn)
+

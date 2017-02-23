54cd0492ba599caa1c21893044f76932698dfbcb
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-23 10:46:48 +0100

    now the register form works, and saves a user with his corresponding userprofile

--- skalvi/views.py
+++ skalvi/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import authenticate, login  # Login module handles sess
 from django.views import generic
 from django.views.generic import View
 from .forms import UserForm, UserProfileForm
+from .models import *
+
 
 
 
@@ -33,19 +35,10 @@ class UserFormView(View):
         form = self.form_class(request.POST)
         profile_form = self.profile_form_class(request.POST)
 
-        print(form)
-        print(profile_form)
-
-
-
         if form.is_valid():
 
             # Take submitted data and save to database
             user = form.save(commit=False)  # temporary saved, not saved in database
-            profile = profile_form.save(commit=False)
-
-            print(user)
-            #print(profile)
 
             # cleaned (normalized) data / formated properly
             email = form.cleaned_data['email']
@@ -53,15 +46,18 @@ class UserFormView(View):
             password = form.cleaned_data['password']
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
+            phone = profile_form.cleaned_data['phone']
+            types = profile_form.cleaned_data['type']
 
             # Make som changes or something useful
             user.set_password(password)
-
-
             user.save()  # saves users to the database
 
+            userProfile = UserProfile(user=user, type=types, phone=phone)
+            userProfile.save()
 
-            #Returns User Object if credentials are correct
+
+            #Returns User Object if credentials are correct and log in.
             user = authenticate(username=username, password=password)
 
             # Check that we got a user back
@@ -73,7 +69,6 @@ class UserFormView(View):
 
         # If not successfully logged in, returns to the same page.
         # Return to the login page
-        print("NEEEI")
         return render(request, self.template_name,
             {
                 'form' : form,

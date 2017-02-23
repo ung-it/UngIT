c2a6bda68d29cd87bec52ce5fa76b0f1fcaafbe3
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-23 10:18:32 +0100

    created two forms one for user, and userprofile

--- skalvi/forms.py
+++ skalvi/forms.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import UserProfile
 
-## Login View
-## Make a blueprint for user forms
+# Login View
+# Make a blueprint for user forms
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-
 
     class Meta:
         model = User
         fields = ['username', "password", "first_name", "last_name", "email"]  # Fields that will appear in form in given order
 
+
+class UserProfileForm(forms.ModelForm):
+    type = forms.CharField(max_length=10)
+
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'type']

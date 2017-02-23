1ec5109b182b81e724977594a02f44f1284428b3
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-23 10:19:57 +0100

    added view to handle register form and use database api to register the user in database

--- skalvi/views.py
+++ skalvi/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login  # Login module handles sessions
 from django.views import generic
 from django.views.generic import View
-from .forms import UserForm
+from .forms import UserForm, UserProfileForm
+
 
 
 def index(request):
@@ -13,22 +14,38 @@ def index(request):
 
 
 class UserFormView(View):
-    form_class = UserForm  ## FormView blueprint
+    form_class = UserForm  # Form View blueprint
+    profile_form_class = UserProfileForm
     template_name = "register.html"  # HTML file that form will appear in
 
     # display blank form
     def get(self, request):
         form = self.form_class(None)  # None represents context
-        return render(request, self.template_name, {'form': form})
+        profile = self.profile_form_class(None)
+        return render(request, self.template_name, {
+            'form': form,
+            'profile': profile
+
+        })
 
     # process form data
     def post(self, request):
         form = self.form_class(request.POST)
+        profile_form = self.profile_form_class(request.POST)
+
+        print(form)
+        print(profile_form)
+
+
 
         if form.is_valid():
+
             # Take submitted data and save to database
-            #
             user = form.save(commit=False)  # temporary saved, not saved in database
+            profile = profile_form.save(commit=False)
+
+            print(user)
+            #print(profile)
 
             # cleaned (normalized) data / formated properly
             email = form.cleaned_data['email']
@@ -37,24 +54,31 @@ class UserFormView(View):
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
 
-
-
+            # Make som changes or something useful
             user.set_password(password)
+
+
             user.save()  # saves users to the database
 
 
-            # Returns User Object if credentials are correct
+            #Returns User Object if credentials are correct
             user = authenticate(username=username, password=password)
 
             # Check that we got a user back
             if user is not None:
                 if user.is_active:
                     login(request, user)
+
                     return redirect('skalvi:index')
 
         # If not successfully logged in, returns to the same page.
         # Return to the login page
-        return render(request, self.template_name, {'form' : form})
+        print("NEEEI")
+        return render(request, self.template_name,
+            {
+                'form' : form,
+                'profile' : profile_form
+            })
 
 
 

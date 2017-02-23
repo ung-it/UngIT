4e4d146cebc2b286ba0b39e2100168d642f2cd24
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-23 11:43:42 +0100

    nothing

--- skalvi/urls.py
+++ skalvi/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'skalvi'
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.UserFormView.as_view(), name="register")

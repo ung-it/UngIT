9d2edb5c3fe43e04d4dea1298c31638fe8c07256
Woldt <thmwld@gmail.com>
2017-02-24 15:36:44 +0100

    facebook login work and relevant info is retrieved. code cleanup is in progress

--- assets/js/FacebookButton.js
+++ assets/js/FacebookButton.js
@@ -1,4 +1,3 @@
-<div className="fb-login-button" data-size="large" data-show-faces="false" data-auto-logout-link="true"></div>
 import React, { Component } from 'react';
 import {Button} from "react-bootstrap";
 
@@ -7,27 +6,32 @@ class FacebookButton extends Component {
         super(props);
 
         this.state = {
+            login: "Log in"
         };
 
-        this.handleClick = this.handleClick.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
         this.checkLoginState = this.checkLoginState.bind(this);
         this.statusChangeCallback = this.statusChangeCallback.bind(this);
         this.testAPI = this.testAPI.bind(this);
+        this.testMe = this.testMe.bind(this);
    }
 
 
     componentDidMount() {
     window.fbAsyncInit= function() {
         FB.init({
-          appId      : '1658650714438155',
-          cookie     : true,  // enable cookies to allow the server to access
+            appId      : '1658650714438155',
+            cookie     : true,  // enable cookies to allow the server to access
                             // the session
-          xfbml      : true,  // parse social plugins on this page
-          version    : 'v2.3' // use version 2.1
+            fields     : 'id,name,email,picture',
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.8' // use version 2.1
         });
     }.bind(this);
 
-        /*
+
+/*
     // Load the SDK asynchronously
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
@@ -36,21 +40,24 @@ class FacebookButton extends Component {
         js.src = "//connect.facebook.net/nb_NO/sdk.js#xfbml=1&version=v2.8&appId=1658650714438155";
         fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-         */
+*/
 
     }
 
     testAPI() {
         console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
+        FB.api('/me', 'GET', {fields: 'name,id,picture,email'}, function(response) {
             console.log('Successful login for: ' + response.name);
             console.log('Thanks for logging in, ' + response.name + '!');
+            console.log(JSON.stringify(response));
+
         });
     }
 
     statusChangeCallback(response) {
         console.log('statusChangeCallback');
         console.log(response);
+        setTimeout("",1000);
         if (response.status === 'connected') {
             this.testAPI();
         } else if (response.status === 'not_authorized') {
@@ -66,10 +73,18 @@ class FacebookButton extends Component {
         }.bind(this));
     }
 
-    handleClick() {
-        FB.login(this.checkLoginState());
+    handleLogin() {
+        //FB.login(this.checkLoginState());
+        FB.login(function(response) {
+          console.log(response);
+        });
     }
-
+    handleLogout() {
+        //FB.login(this.checkLoginState());
+        FB.logout(function(response) {
+            console.log(response);
+        });
+    }
 
     render(){
         return (
@@ -81,6 +96,8 @@ class FacebookButton extends Component {
                     data-show-faces="false"
                     data-auto-logout-link="true">
                 </div>
+                <Button onClick={this.handleLogin}>{this.state.login + " with Facebook"}</Button>
+                <Button onClick={this.handleLogout}>Logout</Button>
                 <Button onClick={this.checkLoginState}>Status</Button>
             </div>
         );

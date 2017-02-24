0750781c5b1d2cd701352a39067f5a69cfe4b635
Martin Stigen <martin.stigen@live.no>
2017-02-24 10:55:45 +0100

    fixed navbar links and rendering of components depending on subpage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ActivitiesContainer from './ActivitiesContainer';
 
-ReactDOM.render(
-    <ActivitiesContainer/>,
-    document.getElementById('activities')
-);
+if (document.getElementById('activities')) {
+    ReactDOM.render(
+        <ActivitiesContainer/>,
+        document.getElementById('activities')
+    );
+}

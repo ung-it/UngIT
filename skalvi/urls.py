46b631e51c1c252ebcf63da58518b3a1a6610186
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-24 12:11:15 +0100

    made login api url use ApiFunction.py file

--- skalvi/urls.py
+++ skalvi/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from . import ApiFunctions
 
 app_name = 'skalvi'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.UserFormView.as_view(), name="register"),
-    url(r'^api/skalvi/login/$', views.loginView, name="register"),
+    url(r'^api/skalvi/login/$', ApiFunctions.loginView, name="login"),
 ]

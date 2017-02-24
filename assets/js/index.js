ade5e33d2f3dfbff31785dd7a0138586e04c39a7
Martin Stigen <martin.stigen@live.no>
2017-02-24 16:08:40 +0100

    fixed routing issue

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import LoginModal from './LoginModal';
 import ActivitiesContainer from './ActivitiesContainer';
 
-ReactDOM.render(
-    <ActivitiesContainer/>,
-    document.getElementById('activities')
-);
-ReactDOM.render(
-    <LoginModal />,
-    document.getElementById('loginComponent')
-);
+if (document.getElementById('activities')) {
+    ReactDOM.render(
+        <ActivitiesContainer/>,
+        document.getElementById('activities')
+    );
+}
+if (document.getElementById('loginComponent')) {
+    ReactDOM.render(
+        <LoginModal />,
+        document.getElementById('loginComponent')
+    );
+}

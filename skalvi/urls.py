b241daef92326a87a0a715a67ece69015518c3cb
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-24 10:30:22 +0100

    added a url to use the loginfunciton

--- skalvi/urls.py
+++ skalvi/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'skalvi'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.UserFormView.as_view(), name="register")
+    url(r'^register/$', views.UserFormView.as_view(), name="register"),
+    url(r'^/api/skalvi/login/$', views.loginView, name="register"),
 ]

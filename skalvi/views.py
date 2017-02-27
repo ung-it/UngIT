38db61ff41e6d9d3ef5079bba223f5fdb0097035
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-02-27 16:47:58 +0100

    changed back to templateresponse on allactivities

--- skalvi/views.py
+++ skalvi/views.py
@@ -140,4 +140,4 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def allactivities(request):
-    return HttpResponse("allActivities.html")
+    return TemplateResponse(request, 'allActivities.html', {})

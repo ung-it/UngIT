b3b8d271e4ab7a9fe4119c15274e54d5cabd1ba3
Martin Stigen <martin.stigen@live.no>
2017-03-01 15:38:27 +0100

    changed project folder structure to be more correct

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('skalvi.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

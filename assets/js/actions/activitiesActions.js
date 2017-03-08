c72d7e6d06d06cd933bc8dc16030a42011779399
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-08 21:59:29 +0100

    added description of class

--- assets/js/actions/activitiesActions.js
+++ assets/js/actions/activitiesActions.js
@@ -4,6 +4,31 @@
 import {getAllActivities} from "../APIFunctions";
 import {getActivityInfo} from '../APIFunctions';
 
+
+/*
+* Action creator and action are two separate things, the action creator is the actual action-function
+*
+* the action is actually the part that gets returned, and consist only of two things, type and payload.
+* type: just a string that explains what action happend, EVERY reducer gets notified with this type when an action
+* occurs.
+*
+* payload: any informasion or extra / new  data needed to perform the action
+*
+* Action creator and action is just a function that is something you can do on your application,
+* like submitting a form, clicking a button, adding a user, etc.
+* If that action is to change some state or database or reducer ( something in the store ) we need actions because
+* they are used to change the application state
+*
+* To use an Action, you need to import it in the file where you want to use it, then hook it up, through a onClick, etc.
+* But to hook it up, you cant just use it like a import function, then you buypass Redux, NOT cool. So -->
+* we need to connect the Action to the containar / component through the connect function and another function called
+*   matchDispatchToProps(dispatch) <-- Pass in the action as a prop (this.prop)
+*   connect(matchDispatchToProps)(className) <-- Make the pass to the class/container/component
+*
+* Dispatch means/is just a fancy way of saying 'call a function'
+*
+* */
+
 export const fetchActivities = () => {
 
        let tempActivities = [];

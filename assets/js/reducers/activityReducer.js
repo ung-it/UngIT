edd52e116d443df1e9390a3d6fc85bf6a25a8821
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-08 22:42:28 +0100

    added two test activities to the reducer

--- assets/js/reducers/activityReducer.js
+++ assets/js/reducers/activityReducer.js
@@ -4,19 +4,54 @@
 
 
 export default function reducer(state={
-    activities: []
-    }, action) {
+    activities:
+        [
+            {
+                id: 23232323,
+                title: 'Sigve',
+                provider: 'Skaugvoll',
+                adaptions: 'Nada',
+                age: '22',
+                location: 'Gård',
+                description: 'Male',
+                price: 'Expensive',
+                date: new Date(),
+                timeStart: new Date(),
+                timeEnd: new Date(),
+                images: null,
+                videos: null
+
+            },
+            {
+                id: 2131323,
+                title: 'Ellen',
+                provider: 'Bakksjo',
+                adaptions: 'Nada',
+                age: '21',
+                location: 'by',
+                description: 'Female',
+                price: 'Expensive',
+                date: new Date(),
+                timeStart: new Date(),
+                timeEnd: new Date(),
+                images: null,
+                videos: null
+
+
+            }
+        ]
+}, action) {
 
     console.log('Activityreducer');
 
 
     switch (action.type) {
-      case "ACTIVITY_FETCHED": {
-        return {
-            ...state,
-            activities: [...state, action.payload]
+        case "ACTIVITY_FETCHED": {
+            return {
+                ...state,
+                activities: [...state, action.payload]
+            }
         }
-      }
     }
     return state
 }

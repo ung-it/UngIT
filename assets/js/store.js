c07a67b518726cf96ba5090ac2feb246ee75d551
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-08 13:59:24 +0100

    commented out redux-middleware that gave webpack compile error

--- assets/js/store.js
+++ assets/js/store.js
@@ -5,7 +5,7 @@ import { applyMiddleware, createStore } from "redux"
 
 // import logger from "redux-logger"
 // import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
+// import promise from "redux-promise-middleware"
 
 import reducer from "./reducers/indexReducer"
 

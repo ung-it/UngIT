55ebdca2f3ccd64a9f68cac71e28748d3b29f5b1
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-09 13:49:41 +0100

    added state to the component to handle the modal view. now working

--- assets/js/components/ActivityPageComponent.js
+++ assets/js/components/ActivityPageComponent.js
@@ -9,6 +9,15 @@ import ActivityModal from '../ActivityModal';
 
 class ActivityPageContainer extends React.Component {
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        }
+    }
+
+
     componentWillMount() {
         this.showMap = this.showMap.bind(this);
         //console.log('mount from ActivityPageContainer');
@@ -21,36 +30,36 @@ class ActivityPageContainer extends React.Component {
     createActivityItem(){
         let poster = null;
         return (
-                <div
-                    key={this.props.activity.id}
+            <div
+                key={this.props.activity.id}
+            >
+                <Thumbnail
+                    className="activityBigStyle"
+                    src={poster}
+                    onClick={this.openActivityModal.bind(this)}
+                    title="Klikk på aktiviteten for mer informasjon"
                 >
-                    <Thumbnail
-                        className="activityBigStyle"
-                        src={poster}
-                        onClick={this.openActivityModal.bind(this)}
-                        title="Klikk på aktiviteten for mer informasjon"
-                    >
-                        <h3>{this.props.activity.title}</h3>
-                        <div className="info-box-wrapper">
-                            <div className="icon-container">
-                                <p><Glyphicon glyph="glyphicon glyphicon-calendar"/></p>
-                                <p><Glyphicon glyph="glyphicon glyphicon-time"/></p>
-                                <p><Glyphicon glyph="glyphicon glyphicon-map-marker"/></p>
-                            </div>
-                            <div className="info-container">
-                                <p>{this.props.activity.date.getDate()}. {this.props.activity.date.getMonth()}</p>
-                                <p>{this.props.activity.timeStart.toDateString()} - {this.props.activity.timeEnd.toDateString()}</p>
-                                <p>{this.props.activity.location}</p>
-                            </div>
-                            <div className="about-container">
-                                <p>{this.props.activity.description}</p>
-                            </div>
+                    <h3>{this.props.activity.title}</h3>
+                    <div className="info-box-wrapper">
+                        <div className="icon-container">
+                            <p><Glyphicon glyph="glyphicon glyphicon-calendar"/></p>
+                            <p><Glyphicon glyph="glyphicon glyphicon-time"/></p>
+                            <p><Glyphicon glyph="glyphicon glyphicon-map-marker"/></p>
                         </div>
-                        <ActivityModal id={this.props.activity.id} show={this.props.activity.show}>test</ActivityModal>
-                    </Thumbnail>
-                </div>
-            );
-        };
+                        <div className="info-container">
+                            <p>{this.props.activity.date.getDate()}. {this.props.activity.date.getMonth()}</p>
+                            <p>{this.props.activity.timeStart.toDateString()} - {this.props.activity.timeEnd.toDateString()}</p>
+                            <p>{this.props.activity.location}</p>
+                        </div>
+                        <div className="about-container">
+                            <p>{this.props.activity.description}</p>
+                        </div>
+                    </div>
+                    <ActivityModal id={this.props.activity.id} show={this.state.show}>test</ActivityModal>
+                </Thumbnail>
+            </div>
+        );
+    };
 
 
 
@@ -72,7 +81,7 @@ class ActivityPageContainer extends React.Component {
     }
 
     openActivityModal() {
-        this.setState({show: true})
+       this.setState({show:true})
     }
 }
 

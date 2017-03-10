f273b044c51a21e034c2d046bd346a9f78e9a77c
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-10 14:41:13 +0100

    removed debug statements, created function to get all activities from db and then set them as the activity reducers initial state.

--- assets/js/store.js
+++ assets/js/store.js
@@ -9,9 +9,53 @@ import { applyMiddleware, createStore } from "redux"
 
 import allReducers from "./reducers/indexReducer"
 
+import {getAllActivitiesAsArrayForReducer} from "./APIFunctions";
+
+
 //const middleware = applyMiddleware(promise());
 
-console.log('store');
-const store = createStore(allReducers);
+
+function getActivitiesFromDB(){
+    let tempArray =[];
+    getAllActivitiesAsArrayForReducer().then((body) =>{
+        body.map((activity) => {
+            let act = activity.fields;
+            act['id'] = activity.pk;
+            let obj = {
+                id: act.id,
+                title: act.title,
+                provider: act.provider,
+                adaptions: act.adaptions,
+                age: act.age,
+                location: act.location,
+                description: act.description,
+                price: act.price,
+                date: new Date(act.date),
+                time_start: act.time_start.substring(0,act.time_start.lastIndexOf(":")),
+                time_end: act.time_end.substring(0,act.time_end.lastIndexOf(":")),
+                images: act.images,
+                videos: act.videos,
+            };
+
+            tempArray.push(obj);
+        });
+    });
+
+    const state = {
+        activity: tempArray
+    };
+
+    return state
+
+}
+
+const initState = getActivitiesFromDB();
+
+
+
+const store = createStore(
+    allReducers,
+    initState
+);
 
 export default store;

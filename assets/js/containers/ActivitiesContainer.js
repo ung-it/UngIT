60a636b5f5bdb0bfdbc2eaa238bda7a3dab66126
ingrskar <ingrskar@stud.ntnu.no>
2017-03-13 10:23:59 +0100

    styling on add activity btn

--- assets/js/containers/ActivitiesContainer.js
+++ assets/js/containers/ActivitiesContainer.js
@@ -41,11 +41,9 @@ class ActivitiesContainer extends Component {
 
         return (
             <div style={styles.activitiesContainerStyle}>
-              <h2>Kommende Aktiviteter</h2>
               <div style={styles.activitiesStyle}>
                   {this.createHomePageComponent()}
               </div>
-                <a href="/activity">Opprett ny aktivitet</a>
             </div>
         );
     }

5eb69430627dae4e520110f24e5fad61f8d8fe76
Christina Hellenes Andresen <christina.h.andresen@gmail.com>
2017-03-23 12:47:45 +0100

    Finished with empty filter button

--- assets/js/components/WeekPicker.js
+++ assets/js/components/WeekPicker.js
@@ -15,7 +15,6 @@ class WeekPicker extends React.Component {
 		this.state = {
 			startDate: moment(),
 			endDate: moment().add(29, 'days'),
-			//date: moment().format('DD/MM/YYYY') + ' - ' + moment().add(29, 'days').format('DD/MM/YYYY')
 		};
 	};
 
@@ -23,55 +22,24 @@ class WeekPicker extends React.Component {
 		this.setState ({
 			startDate: picker.startDate,
 			endDate: picker.endDate,
-			//date: picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'),
 		});
-		let date = picker.startDate.format('YYYY-MM-DD') + "," + picker.endDate.format('YYYY-MM-DD');
-		//const
+		const date = picker.startDate.format('YYYY-MM-DD') + "," + picker.endDate.format('YYYY-MM-DD');
 		this.props.onFilterChange(date);
-		//this.state.date = picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY');
-
 	};
-
-	componentWillReceiveProps(props) {
-		console.log(props);
-		//const date = this.props.activeFilters.split(',').map(a => new Date(a));
-		//this.setState ({
-		//		startDate: moment(date[0]).format('DD/MM/YYYY'),
-		//		endDate: moment(date[1]).format('DD/MM/YYYY'),
-		//	})
-		//this.forceUpdate()
-
-	}
-
 
 
 	render() {
-		//const start = this.state.startDate.format('DD/MM/YYYY');
-		//const end = this.state.endDate.format('DD/MM/YYYY');
-		//let label = moment(this.state.startDate).format('DD/MM/YYYY') + ' - ' + moment(this.state.endDate).format('DD/MM/YYYY');
-		//if (start === end) {
-		////	label = start;
-		//};
-
-		let label = ''
+		let label = '';
 
 		const date = this.props.activeFilters.split(',').map(a => new Date(a));
-		//const startD = date[0];
-		//const label = startD + ' - ' + date[1];
 
-		console.log(this.props.activeFilters);
-
-		if (date[0] == null || date[0] == 'Invalid Date' && date[1] == null || date[1] == 'Invalid Date') {
-			label = moment(this.state.startDate).format('DD/MM/YYYY') + ' - ' + moment(this.state.endDate).format('DD/MM/YYYY');
+		if ((date[0] == null || date[0] == 'Invalid Date') && (date[1] == null || date[1] == 'Invalid Date')) {
+			this.state.startDate = moment();
+			this.state.endDate = moment().add(29, 'days');
+			label = this.state.startDate.format('DD/MM/YYYY') + ' - ' + this.state.endDate.format('DD/MM/YYYY');
 		}
 		else {
-			// not a date
-			const startD = moment(date[0]).format('DD/MM/YYYY');
-			const endD = moment(date[1]).format('DD/MM/YYYY');
-
-			//console.log(startD + ' - ' + endD);
-			label = startD + ' - ' + endD;
-			//date[0].format('DD/MM/YYYY')
+			label = moment(date[0]).format('DD/MM/YYYY') + ' - ' + moment(date[1]).format('DD/MM/YYYY');
 		}
 
 		return (

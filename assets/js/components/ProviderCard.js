3c7113c1962566e568781c2bc4df7853a91a49dc
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-28 12:47:02 +0200

    started styling to look like activityPage

--- assets/js/components/ProviderCard.js
+++ assets/js/components/ProviderCard.js
@@ -1,9 +1,6 @@
 import React from "react"
-import { connect } from "react-redux"
-import { Thumbnail, Glyphicon } from 'react-bootstrap';
-
-
-
+import {connect} from "react-redux"
+import {Thumbnail, Glyphicon} from 'react-bootstrap';
 
 
 const moment = require('moment');
@@ -13,15 +10,44 @@ class ProviderCard extends React.Component {
 
     constructor(props) {
         super(props);
-        
+
     };
 
 
-     createProviderItem = () =>{
-         const provider = JSON.parse(this.props.provider);
-         return provider.Navn
+    createProviderItem = () => {
 
-     };
+        this.props.provider.replace("Kategori(er) ", "Kategorier");
+        const provider = JSON.parse(this.props.provider);
+
+        if(!provider.Navn){
+            return null;
+        }
+        
+
+        return (
+            <div key={provider.Id}>
+                <div
+                    className="activityBigStyle"
+                >
+                    <div className="row">
+                        <div className="col-sm-9">
+                            <h3 className="big-info-header">{provider.Navn}</h3>
+                            <div className="row">
+                                <div classID="big-info-container" className="col-md-3">
+                                    <div className="big-icon-container-div"><Glyphicon
+                                        glyph="glyphicon glyphicon-map-marker"/>{provider.Adresse}</div>
+                                </div>
+                                <div className="col-md-8">
+                                    <p>{provider.Kategorier}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+    };
 
 
     render() {

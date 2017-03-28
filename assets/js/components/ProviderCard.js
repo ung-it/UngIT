02cd457dc64f9714a3a8c6d033e75dd0a5293c2a
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-03-28 12:07:14 +0200

    makes string to json format and shows provider name

---
+++ assets/js/components/ProviderCard.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { connect } from "react-redux"
+import { Thumbnail, Glyphicon } from 'react-bootstrap';
+
+
+
+
+
+const moment = require('moment');
+
+
+class ProviderCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        };
+    };
+
+
+     createProviderItem = () =>{
+         const provider = JSON.parse(this.props.provider);
+         return provider.Navn
+
+     };
+
+
+    render() {
+        return (
+            <div>
+                {this.createProviderItem()}
+            </div>
+        );
+    }
+}
+
+export default ProviderCard;

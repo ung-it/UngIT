0b003a4d7ef654f4c51d7bc82bd110243356a60b
Martin Stigen <martin.stigen@live.no>
2017-03-30 11:05:36 +0200

    Provider dropdown now shows values as wanted

--- assets/js/components/ProviderField.js
+++ assets/js/components/ProviderField.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+
+const styles = {
+    customWidth: {
+        width: 300,
+    },
+};
 
 class ProviderField extends Component {
 
@@ -9,18 +17,39 @@ class ProviderField extends Component {
         super(props);
         this.state = {
             value: 0,
-            providers: ["Meg selv", "En registrert arrangør", "En annen arrangør"]
+            personal: "Meg selv",
+            registered: ["Aktør jeg representerer", "En annen aktør jeg representerer"],
+            unregistered: ["En aktør jeg ikke representerer", "En annen aktør jeg ikke representerer"],
+
+            open: false,
         }
     }
 
     handleChange = (event, index, value) => this.setState({value});
 
+    handleToggle = () => {
+        this.setState({
+            open: !this.state.open,
+        });
+    };
+
+    handleNestedListToggle = (item) => {
+        this.setState({
+            open: item.state.open,
+        });
+    };
+
     render() {
 
-        let items = this.state.providers.map((item, i) => {
+        let items1 = this.state.registered.map((item, i) => {
            return (
-               <MenuItem key={i} primaryText={item} value={i}/>
+               <MenuItem key={item} primaryText={item} value={i+1}/>
            )
+        });
+        let items2 = this.state.unregistered.map((item, i) => {
+            return (
+                <MenuItem key={item} primaryText={item} value={i+1+this.state.registered.length}/>
+            )
         });
 
         return (
@@ -28,8 +57,14 @@ class ProviderField extends Component {
                 floatingLabelText="Velg Arrangør"
                 value={this.state.value}
                 onChange={this.handleChange}
+                autoWidth={false}
+                style={styles.customWidth}
             >
-                {items}
+                <MenuItem primaryText={this.state.personal} value={0}/>
+                <Subheader>Mine aktører</Subheader>
+                {items1}
+                <Subheader>Andre aktører</Subheader>
+                {items2}
             </SelectField>
         )
     }

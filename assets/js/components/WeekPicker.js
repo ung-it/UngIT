e5563b07fcc999c3c99be27238303ac2ff030d94
Christina Hellenes Andresen <christina.h.andresen@gmail.com>
2017-04-01 22:36:09 +0200

    Cleaned code in week picker

--- assets/js/components/WeekPicker.js
+++ assets/js/components/WeekPicker.js
@@ -12,45 +12,15 @@ const moment = require('moment');
 
 class WeekPicker extends React.Component {
 
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			startDate: new Date(),
-		};
-	};
 
 	handleEvent = (event, date) => {
-		this.setState ({
-			startDate: date,
-		});
-		//const date = picker.startDate.format('YYYY-MM-DD') + "," + picker.endDate.format('YYYY-MM-DD');*/
-
 		this.props.onFilterChange(date);
 	};
 
 
 	render() {
-		/*let label = '';*/
-
-
-		//console.log(date)
-
-		/*if ((date[0] == null || date[0] == 'Invalid Date') && (date[1] == null || date[1] == 'Invalid Date')) {
-			this.state.startDate = moment();
-			this.state.endDate = moment().add(182, 'days');
-			label = this.state.startDate.format('DD/MM/YYYY') + ' - ' + this.state.endDate.format('DD/MM/YYYY');
-		}
-		else {
-			label = moment(date[0]).format('DD/MM/YYYY') + ' - ' + moment(date[1]).format('DD/MM/YYYY');
-		}
-
-		<DateRangePicker startDate={this.state.startDate} endDate={this.state.endDate} onEvent={this.handleEvent}>
-	          	<span id="time-period">{label}</span>
-		    	<button type="button" className="btn btn-default"><Glyphicon glyph="glyphicon glyphicon-calendar"/></button>
-			</DateRangePicker>*/
 		return (
-			<DatePicker hintText="Velg en dato.."  mode="landscape" onChange={this.handleEvent} fullWidth={true}/>
+			<DatePicker hintText="Søk på en dato.."  mode="landscape" onChange={this.handleEvent} fullWidth={true}/>
 
 		);
 	};

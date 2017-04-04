e73f1023f298b9a4a5a73333d2c2b5af320fcadd
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-04-04 17:48:13 +0200

    added constructor and sate to maintain the offset for rendering 50+ providers and added button to load more Dynamic loading works, but not sure if it's the REACT way

--- assets/js/components/ProvidersList.js
+++ assets/js/components/ProvidersList.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ProviderCard from './ProviderCard';
 
+import { Button } from "react-bootstrap";
+
 class ProvidersList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            offset: 50,
+        };
+
+    };
+
+
+    loadMore(){
+        this.setState({
+          offset: this.state.offset +=50
+        });
+
+    }
 
     render() {
         const styles = {
@@ -11,20 +30,31 @@ class ProvidersList extends React.Component {
                 flexDirection: "row",
                 justifyContent: "space-around",
                 width: '100%',
-            }
+            },
+
         };
+
 
         // If no providers found
         if (this.props.providers.length < 1) {
             return <h1>Ingen arrangører funnet</h1>;
         }
 
-        const providers = this.props.providers.map(provider =>
+
+
+
+        const prov = this.props.providers.slice(0, this.state.offset);
+
+
+
+
+        const providers = prov.map(provider =>
             <ProviderCard key={provider.Id} provider={provider} id={provider.Id}/>
         );
 
         return <div style={styles.activitiesStyle}>
             {providers}
+            <Button   className="btn-success" onClick={() => this.loadMore()}>Last flere</Button>
         </div>
     }
 }

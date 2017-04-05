3144a854ba7b30362315d0804e2abc02ca33a26d
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-04-05 20:31:44 +0200

    changes that the parsing is only on shown providers, and not ALL in database. this is only for performance enhancment

--- assets/js/components/ProvidersList.js
+++ assets/js/components/ProvidersList.js
@@ -37,7 +37,7 @@ class ProvidersList extends React.Component {
 
                     // Slice the reducer data that is provided to match the offset.
                     const prov = this.props.providers.slice(0, this.state.offset);
-                    const parsedProviders = this.props.providers.map(p => (JSON.parse(p.fields.aktordatabase)));
+                    const parsedProviders = prov.map(p => (JSON.parse(p.fields.aktordatabase)));
                     this.setState({
                         providers: prov.map((provider, k) => {
 

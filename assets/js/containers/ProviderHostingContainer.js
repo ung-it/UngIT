56f783efae8bb190665f1ccf43b3c212e6bf282c
Andreas <andrnors@stud.ntnu.no>
2017-04-19 11:29:21 +0200

    create new container for providerHostingActivities

---
+++ assets/js/containers/ProviderHostingContainer.js
@@ -0,0 +1,96 @@
+/**
+ * Created by Andy on 19-Apr-17.
+ */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Provider, connect} from "react-redux";
+
+import {fetchAllProHostingActivities} from '../actions/activitiesActions';
+import ActivityCardHomePage from '../components/ActivityCardHomePage';
+import configureStore from "../configureStore";
+
+import '../../styles/activityBox.css';
+
+const store = configureStore();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import Provider from "react-redux/src/components/Provider";
+
+class ProviderHostingContainer extends Component {
+
+    createActivityCardComponent = () => {
+        return this.props.proHostingActivities.map(activity => {
+            return (
+                <ActivityCardHomePage
+                    key={activity.id + activity.fields.activityName}
+                    id={activity.pk}
+                    activity={activity.fields}
+                />
+            )
+        });
+    };
+
+    render() {
+        const styles = {
+            activitiesContainerStyle: {
+                padding: "1em"
+            },
+            activitiesStyle: {
+                display: "flex",
+                flexWrap: "wrap",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                padding: "0.5em"
+            },
+        };
+
+        let proHostingContainer = <p>Du har ikke opprettet noen aktiviteter</p>;
+        if (this.props.proHostingActivities.length > 0) {
+            proHostingContainer =
+                <div style={styles.activitiesStyle}>
+                    {this.createActivityCardComponent()}
+                </div>
+        }
+
+        return (
+            <div style={styles.activitiesContainerStyle}>
+                {proHostingContainer}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        proHostingActivities: state.activity.proHostingList
+            .sort((a, b) => new Date(a.fields.date) > new Date(b.fields.date)) // Sort descending based on date
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchProHostingActivities: () => dispatch(fetchAllProHostingActivities()),
+    }
+};
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: '#3F51B5',
+    },
+});
+
+// Fetch initial data for state
+store.dispatch(fetchAllProHostingActivities());
+
+ProviderHostingContainer = connect(mapStateToProps, mapDispatchToProps)(ProviderHostingContainer);
+
+ReactDOM.render(
+    <MuiThemeProvider muiTheme={muiTheme}>
+        <Provider store={store}>
+            <ProviderHostingContainer />
+        </Provider>
+    </MuiThemeProvider>, document.getElementById('proHostingActivities')
+);
+
+

a689f84d7029c2a397b2a83f159dfd121089b2b4
Andreas <andrnors@stud.ntnu.no>
2017-04-20 13:09:10 +0200

    removed print, and fixed redirect of post method fails

--- skalvi/views.py
+++ skalvi/views.py
@@ -660,7 +660,6 @@ class RegisterProfileView(View):
 
     def post(self, request):
         profile_form = self.form_class(request.POST)
-
         if profile_form.is_valid():
             # Take submitted data and save to database
             profile_form.save(commit=False)
@@ -680,7 +679,8 @@ class RegisterProfileView(View):
                                   last_name=last_name, email=email)
             profile.save()
 
-        return redirect("../mypage/" + profile_name)
+            return redirect("../mypage/" + profile_name)
+        return redirect("../mypage/" + request.session["profile_name"])
 
 
 class ProviderView(View):

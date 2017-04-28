00ab7a905318964b6bc5b9b2d51326ce239c3452
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-04-28 12:00:27 +0200

    providersActions.js cleaned, removed \n

--- assets/js/actions/providersActions.js
+++ assets/js/actions/providersActions.js
@@ -1,7 +1,5 @@
 import { getAllProviders } from '../APIFunctions';
 
-
-
 // ACTION TYPES
 export const FETCHED_ALL_PROVIDERS = 'FETCH_ALL_PROVIDERS';
 export const ADD_SEARCH_FOR_FILTER = 'ADD_SEARCH_FOR_FILTER';
@@ -10,9 +8,6 @@ export const TRASH_BUTTON_CLICKED = 'TRASH_BUTTON_CLICKED';
 export const ACTIVITY_BUTTON_CLICKED = 'ACTIVITY_BUTTON_CLICKED';
 export const ADD_SUITED_FOR_FILTER = 'ADD_SUITED_FOR_FILTER';
 export const SUITED_FOR_BUTTON_CLICKED = 'SUITED_FOR_BUTTON_CLICKED';
-
-
-
 
 // ACTION CREATORS
 export function fetchedAllProviders(providers) {
@@ -60,7 +55,6 @@ export function trashButtonClicked() {
         type: TRASH_BUTTON_CLICKED,
     }
 }
-
 
 // HELPING METHODS
 export function fetchAllProviders() {

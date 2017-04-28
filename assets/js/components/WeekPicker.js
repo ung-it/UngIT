711cfcea350be574b8a4a88da14cb32cad736243
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-04-28 12:53:30 +0200

    cleaned WeekPicker

--- assets/js/components/WeekPicker.js
+++ assets/js/components/WeekPicker.js
@@ -1,70 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Thumbnail, Glyphicon } from "react-bootstrap";
+import {Thumbnail, Glyphicon} from "react-bootstrap";
 import DatePicker from 'material-ui/DatePicker';
 import areIntlLocalesSupported from 'intl-locales-supported';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 import '../../styles/daterangepicker.css'
 
+injectTapEventPlugin();
 
 const moment = require('moment');
-
 let DateTimeFormat;
 
-/**
- * Use the native Intl.DateTimeFormat if available, or a polyfill if not.
- */
 if (areIntlLocalesSupported(['nb-NO'])) {
     DateTimeFormat = global.Intl.DateTimeFormat;
-} else {
+}
+else {
     const IntlPolyfill = require('intl');
     DateTimeFormat = IntlPolyfill.DateTimeFormat;
     require('intl/locale-data/jsonp/nb-NO');
 }
 
-
 class WeekPicker extends React.Component {
 
+    handleEvent = (event, date) => {
+        this.props.onFilterChange(date);
+    };
 
-	handleEvent = (event, date) => {
-		this.props.onFilterChange(date);
-	};
+    formatDate = (date) => {
+        return date.getDate() + " / " + (date.getMonth() + 1) + " / " + date.getFullYear();
+    };
 
-	formatDate = (date) => {
-		return date.getDate() + " / " + (date.getMonth() + 1) + " / " + date.getFullYear();
-	};
+    render() {
+        let date = {};
+        if (new Date(this.props.activeFilters) == 'Invalid Date') {
+            date = {};
+        } else {
+            date = new Date(this.props.activeFilters);
+        }
 
-
-	render() {
-		let date = {};
-		if (new Date (this.props.activeFilters) == 'Invalid Date') {
-			date = {};
-		} else {
-			date = new Date(this.props.activeFilters);
-		}
-
-		return (
-			<DatePicker
+        return (
+            <DatePicker
                 DateTimeFormat={DateTimeFormat}
                 locale="nb-NO"
-				hintText="Søk på en dato.."
+                hintText="Søk på en dato.."
                 cancelLabel="Lukk"
-				mode="landscape"
-				value={date}
-				onChange={this.handleEvent}
-				fullWidth={true}
-				formatDate={this.formatDate}
-				className = "filterItem"
-			/>
-		);
-	};
+                mode="landscape"
+                value={date}
+                onChange={this.handleEvent}
+                fullWidth={true}
+                formatDate={this.formatDate}
+                className="filterItem"
+            />
+        );
+    };
 };
 
 WeekPicker.propTypes = {
-	onFilterChange: React.PropTypes.func.isRequired,
-	activeFilters: React.PropTypes.string,
+    onFilterChange: React.PropTypes.func.isRequired,
+    activeFilters: React.PropTypes.string,
 };
 
 export default WeekPicker;

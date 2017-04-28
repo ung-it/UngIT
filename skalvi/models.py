a6b3d5400b618f10f74ecd1c810fc7beb676f54a
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-04-28 15:40:51 +0200

    cleaned and removed unused code

--- skalvi/models.py
+++ skalvi/models.py
@@ -1,33 +1,20 @@
-# Create your models here.
-
 from django.db import models
 from django.contrib.auth.models import Permission, User
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from jsonfield import JSONField
 
-########### USER PROFILES that extends the auth.models.User table
-
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')  # set the relation / extension to the user model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     type = models.CharField(max_length=5)  # A = admin, P = parent, C = child, etc.
     profile_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=60, null=False, blank=False)
-    phone = models.CharField(max_length=8, null=True, blank=True)  # 90 90 99 09 <-- gives length 8
+    phone = models.CharField(max_length=8, null=True, blank=True)
 
     is_active = models.BooleanField(default=False)
     provider = models.TextField(default="", blank=True)  # contains all found information
 
-
     def __str__(self):
         return self.profile_name + " " + " " + self.last_name + " - Email: " + self.email
-
-
-
-########### END USER PROFILE
 
 class Activity(models.Model):
     activityName = models.CharField(max_length=80)
@@ -59,14 +46,10 @@ class Activity(models.Model):
     def __str__(self):
         return self.activityName
 
-
 class Organisation(models.Model):
-    # not every provider has a org.number, but most do. to handle this we use
-    # auto increment, thus not specifying primary key, and lets Django handle this.
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # set the relation / extension to the user model
-    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)  # set the relation / extension to the user model
-    aktordatabase = JSONField()  # contains all found information
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
+    aktordatabase = JSONField()
 
     def __str__(self):
         if self.userprofile:
@@ -75,8 +58,6 @@ class Organisation(models.Model):
             return self.aktordatabase['Navn']
         else:
             return 'Mangler informasjon'
-
-
 
 class Commentary(models.Model):
     activityId = models.ForeignKey(Activity, on_delete=models.CASCADE)
@@ -87,11 +68,6 @@ class Commentary(models.Model):
     date = models.DateField()
     time = models.TimeField()
 
-
-###########  Relations tables  ##########
-
-
-
 class ParticipateIn(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     activityId = models.ForeignKey(Activity, on_delete=models.CASCADE)
@@ -100,18 +76,13 @@ class ParticipateIn(models.Model):
     def __str__(self):
         return self.activityId.activityName + " - " + self.user_profile_id.profile_name + " - Email: " + self.user_profile_id.email
 
-
 class Hosts(models.Model):
     activityId = models.OneToOneField(Activity, on_delete=models.CASCADE)
     adminId = models.ForeignKey(User, on_delete=models.CASCADE)
     profileId = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    # orgNr = models.ForeignKey(Organisation, on_delete=models.CASCADE) <-- should get the same information through the
-    # admin user reference.
 
     def __str__(self):
         return str(self.activityId.activityName + " hosted by " + self.adminId.username + " : " + self.profileId.profile_name + " " + self.profileId.last_name)
-
-
 
 class EmployedIn(models.Model):
     orgNr = models.ForeignKey(Organisation, on_delete=models.CASCADE)
@@ -121,7 +92,3 @@ class Follows(models.Model):
     orgId = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     user_profile_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-
-    # def __str__(self):
-    #     return str(self.user_profile_id.email + " follows " + self.orgId.aktordatabase)
-

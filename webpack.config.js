982be527706509ffe176b593f8bac71dc7357633
Sigve Skaugvoll <skaugvoll@gmail.com>
2017-05-01 16:27:43 +0200

    trying to get react to use production mode

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify('development'),
+                NODE_ENV: JSON.stringify('production'),
             }
         }),
         new webpack.HotModuleReplacementPlugin(),
